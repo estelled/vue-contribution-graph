37589fd4be7a1883771b2ee70a4accf36a6dd547
estelled <ms.estelle.dumas@gmail.com>
2018-11-04 12:33:26 +0100

    color dynamic

--- src/components/ContributionGraph.vue
+++ src/components/ContributionGraph.vue
@@ -2,7 +2,10 @@
   <div id="graph">
     <button v-on:click="showCont()">show contributions</button>
     <p>{{ activityDict}}<p>  
-    <p>num colonne : {{numCol}}  last row : {{ lastRow }}</p>
+    <div v-if="activityDict[id2date(idCalc(1,1))]">
+    <p> to color : {{ colors[toValue(activityDict[id2date(idCalc(1,1))])] }}</p>
+    <div class="circle" v-bind:style="{ 'background-color': colors[toValue(activityDict[id2date(idCalc(1,1))])] }">the color</div>
+    </div>
     <p>{{contTable}}</p>
 
     <div id="monthRow">
@@ -19,7 +22,11 @@
       <div id="canvas">
         <div class="graph-col" v-for="i in numCol" :key="i"> <!--starts from 1 ! -->
           <div class="graph-row" v-for="j in numRow(i)" :key="j"> 
-            <div class= "circle" v-bind:id="idCalc(i,j)"></div>
+              <div v-if="activityDict[id2date(idCalc(i,j))]">
+                <div class="circle" v-bind:id="idCalc(i,j)" v-bind:style="{ 'background-color': colors[toValue(activityDict[id2date(idCalc(i,j))])] }"></div>
+              </div>     
+              <div v-else class= "circle" v-bind:id="idCalc(i,j)"></div>
+
             <b-tooltip v-bind:target="idCalc(i,j).toString()">
               <div v-if="activityDict[id2date(idCalc(i,j))]"> 
                 {{activityDict[id2date(idCalc(i,j))]}} contributions on {{id2date(idCalc(i,j))}}
@@ -78,6 +85,21 @@ export default {
       }
       return row;
     },
+    toValue: function(contNumber) {
+      if (contNumber == 0 || this.contMax == 0) {
+        return null;
+      }
+      var ratio = contNumber / this.contMax * 100;
+      if (ratio <= 25) {
+        return 0;
+      } else if (ratio <= 50) {
+        return 1;
+      } else if (ratio <= 75) {
+        return 2;
+      } else {
+        return 3;
+      }
+    },
   },
   computed: {
     // the last row has either 1 or 2 days, depending on leap years.
@@ -134,7 +156,16 @@ export default {
         months[i] = monthTable[mIndex];
       }
       return months;
-    }
+    },
+    contMax: function() {
+      var length = this.contTable.length;
+      var max = 0;
+      for (let i = 0; i < length; i++) {
+        var temp = this.contTable[i][1];
+        max = Math.max(temp, max);
+      }
+      return max;
+    },
   }
 };
 </script>
